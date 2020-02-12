fad94ca7af814eeababeffc7746b06b1c90adbaf
JohnBlack19 <37846209+JohnBlack19@users.noreply.github.com>
2020-02-12 02:03:08 -0300

    discord button and others

---
+++ site/index.php
@@ -0,0 +1,165 @@
+<html>
+    <head>
+        <title>••• DenilFleck - RP •••</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="DenilFleck - RP •> Venha viver sua  vida em uma realidade alternativa!">
+		<meta name="keywords" content="DenilFleck, sa-mp, samp, rp, roleplay">
+        <meta name="author" content="Atentivo Technologies">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Muli|Open+Sans+Condensed:300|Questrial&display=swap" rel="stylesheet">
+        <!-- font-family: 'Open Sans Condensed', sans-serif;
+        font-family: 'Muli', sans-serif;
+        font-family: 'Indie Flower', cursive;
+        font-family: 'Questrial', sans-serif; -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    </head>
+    <body>
+        <div id="aviso">
+            <i  id="closeaviso" class="fas fa-times close"></i>
+            <h1>AVISO</h1>
+            <h3>Portal em desenvolvimento. <br><br>Nós entendemos que você está super ancioso para utilizar o portal e nós também estamos, acredite!<br><br> Nós (John_Black e Charlie_Harper) em conjunto com toda staff, estamos trabalhando duro e em breve após a vBeta o portal funcionará com diversas funcionalidades e integrações com o jogo.</h3>
+        </div>
+        <div class="load"> <img src="Images/loading.gif"></div>
+        <div id="staffs">
+            <i  id="close" class="fas fa-times close"></i>
+            <h1>STAFFs</h1>
+            <h3 class="fundador">Fundadores</h3>
+            <h4 class="player">Charlie_Harper</h4>
+            <h4 class="player">John_Black</h4>
+            <h3 class="senior">Sêniors</h3>
+            <h4 class="player">Mateus_Marinheiro</h4>
+            <h3 class="administrador">Administradores(as)</h3>
+            <h4 class="player">Ulisses_Pedroso</h4>
+            <h4 class="player">Emily_Parker</h4>
+            <h4 class="player">John_Philip</h4>
+            <h3 class="fiscalizador">Fiscalizadores(as)</h3>
+            <h4 class="player">Tonny_Riviera</h4>
+            <h3 class="ajudante">Ajudantes</h3>
+            <p>Obs.: Não temos vaga na STAFF no momento.</p>
+        </div>
+        <div class="content-one">
+            <a href="https://discord.gg/HCdQ3vT"><img src="Images/discord.png" id="discordbutton"></a>
+            
+            <div id="background-mobile-menu">
+                <i id="openmenu" class="fas fa-bars"></i>
+                <i id ="closemenu" class="fas fa-times"></i>
+            </div>
+            <div id="menu">
+                <ul>
+                    <a href="https://denilfleckrp.forumeiros.com/" target="_blank"><li>FÓRUM</li></a>
+                    <li>HISTÓRIA</li>
+                    <li id="opendialogstaff">STAFF</li>
+                </ul>
+            </div>
+            
+            <div id="logo"></div>
+            <div id="buttons">
+                <button id="blue">LOGAR-SE</button>
+                <button id="green">REGISTRAR-SE</button>
+            </div>
+            </div>
+            <div class="content-text">
+                <h1>INTRODUÇÃO</h1>
+                <p>Olá! Seja bem-vindo(a) à DENILFLECK ROLEPLAY.</p>
+                <p>Somos um servidor de San Andreas Multiplayer (SA-MP) e atualmente estamos na versão 0.3.DL. Nós somos uma comunidade de jogadores que cultiva o roleplay como essência da jogabilidade em nosso servidor. Com isso eu quero dizer que tudo aquilo que acontece no jogo faz parte de uma interpretação, ou ainda, um paralelo à vida real. Portanto, é de extrema importância, nesse caso, que você compreenda o máximo sobre roleplay para que possa ter uma boa jogabilidade em nosso servidor. Esperamos que se divirta muito conosco, nos vemos lá dentro!</p>
+            </div>
+            <div class="content-two">
+                <h1>Vídeos do servidor</h1>
+                <div id="background-movies">
+                    <i class="fas fa-chevron-left" id="arrow-prev"></i>
+					<i class="fas fa-chevron-right" id="arrow-next"></i>
+                    <div id="action-slick">
+                        <iframe src="https://www.youtube.com/embed/meWpd7uxqTs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="video"></iframe>
+                        <!-- <iframe  src="https://www.youtube.com/embed/meWpd7uxqTs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="video"></iframe>
+                
+                        <iframe  src="https://www.youtube.com/embed/meWpd7uxqTs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="video"></iframe>
+                        <iframe  src="https://www.youtube.com/embed/meWpd7uxqTs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="video"></iframe> -->
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="content-three">
+    
+            </div> -->
+
+            <footer>
+                    <p>Copyright © DenilFleck-RP 2015. Todos os direitos reservados. <strong>by:</strong> Atentivo Technologies</p>
+            </footer>
+        <!-- JavaScript -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+        
+        <script>
+                $(document).ready(function() {
+                $('.load').hide();
+                });
+        </script>    
+    
+		<script type="text/javascript">
+			$(function(){
+				$("#action-slick").slick({
+					infinite:true,
+					slidesToShow:3,
+					slidestoScroll:1,
+					prevArrow:$("#arrow-prev"),
+					nextArrow:$("#arrow-next"),
+                        responsive: [{
+					      breakpoint: 620,
+					      settings: {
+					        slidesToShow: 1
+					      }
+					    }]
+				});
+			})
+
+            function disableScrolling(){
+                var x=window.scrollX;
+                var y=window.scrollY;
+                window.onscroll=function(){window.scrollTo(x, y);};
+            }
+
+            function enableScrolling(){
+                window.onscroll=function(){};
+            }
+
+            function openmenumobile () {
+                var elemento = document.getElementById('menu');
+                elemento.style.display = 'block';
+                if (window.getComputedStyle(elemento).display === "block") {
+                    document.getElementById('openmenu').style.display = 'none';
+                    document.getElementById('closemenu').style.display = 'block';
+                    disableScrolling();
+                }
+            }
+
+            function closemenumobile () {
+                var elemento = document.getElementById('menu');
+                elemento.style.display = 'none';
+                if (window.getComputedStyle(elemento).display === "none") {
+                    document.getElementById('openmenu').style.display = 'block';
+                    document.getElementById('closemenu').style.display = 'none';
+                    window.onscroll=function(){};
+                }
+
+            }
+
+            document.getElementById('openmenu').addEventListener("click", () => openmenumobile());
+            document.getElementById('closemenu').addEventListener("click", () => closemenumobile());
+
+            // OPENS
+            document.getElementById('opendialogstaff').addEventListener("click", () => document.getElementById('staffs').style.display = 'block');
+            document.getElementById('blue').addEventListener("click", () => document.getElementById('aviso').style.display = 'block');
+            document.getElementById('green').addEventListener("click", () => document.getElementById('aviso').style.display = 'block');
+            // CLOSES
+            document.getElementById('close').addEventListener("click", () => 
+            document.getElementById('staffs').style.display = 'none');
+            document.getElementById('closeaviso').addEventListener("click", () => document.getElementById('aviso').style.display = 'none');
+
+		</script>
+        <script type="text/javascript" src="http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    </body>
+</html>
